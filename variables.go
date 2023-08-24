f3d662fba34e44dcbac0d30bc7038dd2109b2baa
Jie Burillo <benjieburillo10@gmail.com>
2023-08-24 11:07:17 +0800

    variables

---
+++ variables.go
@@ -0,0 +1,42 @@
+package main
+
+/*
+// Variables
+
+// global variables
+var x = 10
+var (
+	name             = "Foo"
+	firstName string = "Jie"
+	lastName string
+)
+
+// constants
+const version = 1
+
+func main() {
+// local variables
+	version := 1           // infer int
+	otherVersion := "Bar"  // infer string
+	anotherVersion := 10.1 // infer float32 a float64
+
+	fmt.Println(version)
+	fmt.Println(otherVersion)
+	fmt.Println(anotherVersion)
+}
+
+
+// variable declarations
+var (
+	floatVar32 float32 = 0.1
+	floatVar64         = 0.1
+	name               = "Foo"
+	intVar             = -1
+	uintVar    uint    = 1
+	uin32Var   uint32  = 1
+	uin64Var   uint64  = 10
+	uint8Var   uint8   = 0x1
+	byteVar    byte    = 0x2
+	runVar             = 'a'
+)
+*/
